24fc96330a74fe4150c2c2e4b87d5b247251e57a
Soumo Sarkar <soumosarkar297@gmail.com>
2025-03-13 23:51:08 +0530

    Create exports.zsh

---
+++ zsh/zsh/exports.zsh
@@ -0,0 +1,14 @@
+# Environment variables
+export PATH=$PATH:/usr/local/go/bin
+
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
+export EDITOR=nvim
+
+export PATH=$PATH:/opt/gradle/gradle-8.12.1/bin
+
+
+# AWS devops access key setup
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
