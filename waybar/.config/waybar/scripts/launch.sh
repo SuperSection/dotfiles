5c5ff067338b8c73dd7554664060e2d80f8af41a
Soumo Sarkar <soumosarkar297@gmail.com>
2025-08-14 09:04:30 +0530

    modify waybar launch script and configurations

--- waybar/.config/waybar/scripts/launch.sh
+++ waybar/.config/waybar/scripts/launch.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
+#                    __
+#  _    _____ ___ __/ /  ___ _____
+# | |/|/ / _ `/ // / _ \/ _ `/ __/
+# |__,__/\_,_/\_, /_.__/\_,_/_/
+#            /___/
+#
 
-killall -9 waybar
+# -----------------------------------------------------
+# Prevent duplicate launches: only the first parallel
+# invocation proceeds; all others exit immediately.
+# -----------------------------------------------------
 
+exec 200>/tmp/waybar-launch.lock
+flock -n 200 || exit 0
+
+
+# -----------------------------------------------------
+# Quit all running waybar instances
+# -----------------------------------------------------
+
+killall waybar || true
+pkill waybar || true
+
+
+# Load the waybar
 waybar &
+
+
+# Explicitly release the lock (optional) -> flock releases on exit
+flock -u 200
+exec 200>&-
