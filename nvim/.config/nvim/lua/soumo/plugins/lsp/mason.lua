1bce4c015b9d6439ca5f459f29ea0c29763d8c5b
Soumo Sarkar <soumosarkar297@gmail.com>
2025-08-15 10:41:33 +0530

    comment out `asm_lsp` from mason.lua

--- nvim/.config/nvim/lua/soumo/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/soumo/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "asm_lsp",
+        -- "asm_lsp",
         "ts_ls",
         "html",
         "cssls",
