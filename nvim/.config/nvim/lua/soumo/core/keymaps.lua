d2b3d51c4f9ef49c33a3e83c3741dadb3ad4f23d
Soumo Sarkar <soumosarkar297@gmail.com>
2025-08-20 19:51:46 +0530

    add keymap for opening terminal in new tab or horizontally

--- nvim/.config/nvim/lua/soumo/core/keymaps.lua
+++ nvim/.config/nvim/lua/soumo/core/keymaps.lua
@@ -45,15 +45,17 @@ keymap.set("n", "<C-m>", "<C-i>", opts)
 -- New tab
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabnext<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabprev<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>tn", "<cmd>tabnext<CR>", { desc = "Go to next tab" }) -- go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabprev<CR>", { desc = "Go to previous tab" }) -- go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) -- move current buffer to new tab
+keymap.set("n", "<leader>tt", "<cmd>tabnew | term<CR>", { desc = "Open terminal in new tab" }) -- open terminal in new tab
 
 -- Window Management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<C-`>", "<cmd>split | term<CR>", { desc = "Open terminal in a horizontal split" }) -- open terminal horizontally
 
 -- Move window
 keymap.set("n", "sh", "<C-w>h")
