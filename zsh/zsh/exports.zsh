cd57cd2f4ae5a38e3a7ec24fce05d36e2388cc1f
Soumo Sarkar <soumosarkar297@gmail.com>
2025-08-23 09:23:02 +0530

    refactor zsh exports
    - Add FZF key bindings
    - Refactor golang path
    - Add local scripts to the PATH
    - Add TMUX_CONF path
    - Add local mason bin

--- zsh/zsh/exports.zsh
+++ zsh/zsh/exports.zsh
@@ -1,10 +1,22 @@
-# Environment variables
-export PATH=$PATH:/usr/local/go/bin
+export LANG=en_US.UTF-8
 
+# Add local ~/scripts to the PATH
+export PATH="$HOME/scripts:$PATH"
+
+# Mason
+export PATH="$HOME/.local/share/nvim/mason/bin:$PATH"
+
+# Tmux
+export TMUX_CONF=~/.tmux.conf
+
+# Golang
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$(go env GOPATH)/bin
+
 
 export EDITOR=nvim
+
 
 export PATH=$PATH:/opt/gradle/gradle-8.12.1/bin
 
@@ -14,4 +26,23 @@ export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
 
 
+# flatpak
 export XDG_DATA_DIRS=:/var/lib/flatpak/exports/share:/home/supersection/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/home/supersection/.local/share/flatpak/exports/share
+
+
+# ------------FZF--------------
+# Set up fzf key bindings and fuzzy completion
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git "
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+
+export FZF_DEFAULT_OPTS="--height 50% --layout=default --border --color=hl:#2dd4bf"
+
+# Setup fzf previews
+export FZF_CTRL_T_OPTS="--preview 'bat --color=always -n --line-range :500 {}'"
+export FZF_ALT_C_OPTS="--preview 'eza --icons=always --tree --color=always {} | head -200'"
+
+# fzf preview for tmux
+export FZF_TMUX_OPTS=" -p90%,70% "  
+# -----------------------------
+
